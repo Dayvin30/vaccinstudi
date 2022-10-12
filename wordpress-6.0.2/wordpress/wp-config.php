959f4534ae2755f960efffd3a6f89aff40352801
Dayvin30 <92032342+Dayvin30@users.noreply.github.com>
2022-10-12 22:09:55 +0200

    update

--- wordpress-6.0.2/wordpress/wp-config.php
+++ wordpress-6.0.2/wordpress/wp-config.php
@@ -20,17 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-//mysql://b499eae5ef0a25:728b1d51@eu-cdbr-west-03.cleardb.net/heroku_24407b1830caad3?reconnect=true
-define( 'DB_NAME', 'heroku_24407b1830caad3' );
+define( 'DB_NAME', 'wp_covid' );
 
 /** Database username */
-define( 'DB_USER', 'b499eae5ef0a25' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '728b1d51' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', 'eu-cdbr-west-03.cleardb.net' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
