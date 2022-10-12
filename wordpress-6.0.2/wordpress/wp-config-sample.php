4978e97a3197a5461503b6978d952ba1e57411c5
Dayvin30 <92032342+Dayvin30@users.noreply.github.com>
2022-10-12 16:46:52 +0200

    wp-config.php

--- wordpress-6.0.2/wordpress/wp-config-sample.php
+++ wordpress-6.0.2/wordpress/wp-config-sample.php
@@ -20,16 +20,18 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+
+//mysql://b499eae5ef0a25:728b1d51@eu-cdbr-west-03.cleardb.net/heroku_24407b1830caad3?reconnect=true
+define( 'DB_NAME', 'heroku_24407b1830caad3' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'b499eae5ef0a25' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', '728b1d51' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'eu-cdbr-west-03.cleardb.net' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
