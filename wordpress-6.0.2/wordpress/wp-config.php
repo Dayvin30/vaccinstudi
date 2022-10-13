6fca7d03a633a4c84c537d73caaa74fe9de5e13a
Dayvin30 <92032342+Dayvin30@users.noreply.github.com>
2022-10-13 16:01:35 +0200

    Update wp-config.php

--- wordpress-6.0.2/wordpress/wp-config.php
+++ wordpress-6.0.2/wordpress/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress reset' );
+define( 'DB_NAME', 'FKYr42ryKt' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'FKYr42ryKt' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'FNZNN9i6BH' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'https://remotemysql.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
